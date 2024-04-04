b79dd665e40588ab4ff6ffaed315b4f8166fffc5
Tõnu Tarve <tonu.tarve99@hotmail.com>
2024-04-04 12:58:17 +0300

    php templating

--- index.php
+++ index.php
@@ -1,5 +1,35 @@
 <?php
-
-echo "Hello World!";
-
+$name = 'Tõnu';
+$fruits = ['Apple', 'Banana', 'Cherry', 'Pear'];
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello
+        <? $name ?>
+    </h1>
+    <ul>
+        <?php foreach ($fruits as $fruit) : ?>
+        <?php if ($fruit != 'Apple') : ?>
+            <li>
+                <?= $fruit ?>
+            </li>
+            <?php else : ?>
+            <li>
+                <i>
+                    <? $fruit ?>
+                </i>
+            </li>
+        <?php endif; ?>
+        <?php endforeach; ?>
+    </ul>
+</body>
+
+</html>
